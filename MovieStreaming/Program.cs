4865a2fff78fd05bb3792dc26100dde93327539e
Andrii Litoshko <xxoro.lan@gmail.com>
2016-03-12 18:06:53 +0200

    Module 1

--- MovieStreaming/Program.cs
+++ MovieStreaming/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Akka.Actor;
 
 namespace MovieStreaming
 {
     class Program
     {
+        private static ActorSystem MovieStreamingActorSystem;
+
         static void Main(string[] args)
         {
+            MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+
+            Console.ReadLine();
+
+            MovieStreamingActorSystem.Terminate();
         }
     }
 }
